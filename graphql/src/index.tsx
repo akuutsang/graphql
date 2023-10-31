bcbb44684070194a9ff8e78d2e1f71d448ec1ca2
akutsang <akuutsang@gmail.com>
2023-10-31 04:38:38 +0100

    introduction to graphql

---
+++ graphql/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+
+let client = new ApolloClient({
+  uri : 'https://api.spacex.land/graphql/',
+  cache : new InMemoryCache
+})
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
+
